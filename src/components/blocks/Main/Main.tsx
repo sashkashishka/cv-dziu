6ae8d7ec7983983027b689b5f0595c63ee3e73ca
randomtext <randomtext95@gmail.com>
2022-03-19 07:29:23 +0200

    fix behance

--- src/components/blocks/Main/Main.tsx
+++ src/components/blocks/Main/Main.tsx
@@ -48,7 +48,7 @@ export const MainBlock: React.FC = () => {
           <div className={styles.sliderContainer}>
             <Card
               title="Профиль Behance"
-              url="be.net/randomtext2cb5"
+              url="https://www.behance.net/randomtext2cb5"
               icon={behanceIcon}
               hasAwards
             />
