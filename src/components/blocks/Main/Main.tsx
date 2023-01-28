bc32fe953c0a0533b7566924d88382eb43a3dcd1
randomtext95 <randomtext95@gmail.com>
2023-01-28 19:17:55 +0200

    «correct-behance-link»

--- src/components/blocks/Main/Main.tsx
+++ src/components/blocks/Main/Main.tsx
@@ -46,7 +46,7 @@ export const MainBlock: React.FC = () => {
             <div className={styles.sliderContainer}>
               <Card
                 title="Behance profile"
-                url="be.net/randomtext2cb5"
+                url="https://www.behance.net/randomtext2cb5"
                 icon={behanceIcon}
                 type="behance"
               />
