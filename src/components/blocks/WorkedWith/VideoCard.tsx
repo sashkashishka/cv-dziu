ecfb3daf4a5c183cf29bd5f8bba8be28ac0f2742
randomtext95 <randomtext95@gmail.com>
2023-06-06 10:50:30 +0300

    add link

--- src/components/blocks/WorkedWith/VideoCard.tsx
+++ src/components/blocks/WorkedWith/VideoCard.tsx
@@ -8,40 +8,32 @@ import dziuImage from '../../../images/bg-shapes/dziu.svg';
 import styles from './VideoCard.module.css';
 
 export const VideoCard: React.FC = () => {
-  return (
-    <div className={cn('videoCard', styles.videoCard)}>
-      <div className={cn(styles.videoCV, 'gpu')}>
-        <img src={cameraIcon} alt="camera" />
-        Video CV
-      </div>
+	return (
+		<a
+			href='https://youtu.be/pTLDWFWWlnY'
+			target='_blank'
+			className={cn('videoCard', styles.videoCard)}>
+			<div className={cn(styles.videoCV, 'gpu')}>
+				<img src={cameraIcon} alt='camera' />
+				Video CV
+			</div>
 
-      <img
-        src={dziuImage}
-        alt="Oleksii Dziubak"
-        className={cn(styles.mainImage)}
-      />
+			<img src={dziuImage} alt='Oleksii Dziubak' className={cn(styles.mainImage)} />
 
-      <div className={cn(styles.videoControls, 'gpu')}>
-        <div>
-          <p className={cn(styles.videoName)}>
-            About me
-          </p>
+			<div className={cn(styles.videoControls, 'gpu')}>
+				<div>
+					<p className={cn(styles.videoName)}>About me</p>
 
-          <div className={cn(styles.trackContainer)}>
-            <div className={cn(styles.time)}>00:00</div>
-            <div className={cn(styles.track)}> </div>
+					<div className={cn(styles.trackContainer)}>
+						<div className={cn(styles.time)}>00:00</div>
+						<div className={cn(styles.track)}> </div>
 
-            <div className={cn(styles.time)}>00:30</div>
-          </div>
-        </div>
+						<div className={cn(styles.time)}>00:30</div>
+					</div>
+				</div>
 
-        <img
-          src={youtubeIcon}
-          alt="youtube"
-          width="57px"
-          height="40px"
-        />
-      </div>
-    </div>
-  );
-}
+				<img src={youtubeIcon} alt='youtube' width='57px' height='40px' />
+			</div>
+		</a>
+	);
+};
